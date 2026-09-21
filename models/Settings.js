31d58eb7e80e43d0314bc602ea2978c3991b677b
vardhan-077 <vishnuvardhandodda187@gmail.com>
2026-09-21 18:33:26 +0530

    Add files via upload

--- models/Settings.js
+++ models/Settings.js
@@ -48,6 +48,8 @@ const settingsSchema = new mongoose.Schema(
       ],
     },
 
+    donationTargetAmount: { type: Number, default: 500000 },
+
     bankDetails: {
       accountName: { type: String, default: 'Sri Temple Trust' },
       accountNumber: { type: String, default: '000000000000' },
